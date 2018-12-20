f36424659ccb978d644653717f399b383d45348b
2244835800 <2244835800@qq.com>
2018-12-20 14:32:54 +0800

    MOD：更换了最新的ide

--- src/main/java/com/jingyan/StartApplication.java
+++ src/main/java/com/jingyan/StartApplication.java
@@ -2,10 +2,17 @@ package com.jingyan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class StartApplication {
-
+	@GetMapping(value = "/")
+	public String test(Model model) {
+		return "test";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(StartApplication.class, args);
 	}
